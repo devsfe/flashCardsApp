4256a500f0b55cf5197cb3524338ec073e99be99
Fernando Chaves <fernando.chaves16@hotmail.com>
2021-02-10 05:31:27 -0300

    close add card content function created

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const openBtn = document.getElementById('open-btn');
+const closeBtn = document.getElementById('close-btn')
 const addCardContainer = document.getElementById('add-card-container');
 
 
@@ -6,4 +7,9 @@ const addCardContainer = document.getElementById('add-card-container');
 // open add card container
 openBtn.addEventListener('click', () => {
     addCardContainer.classList.add('active');
+});
+
+// close add card container
+closeBtn.addEventListener('click', () => {
+    addCardContainer.classList.remove('active');
 });
