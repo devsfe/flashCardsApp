3a3267953a558e07efe28d1cab193dbdff5aa195
Fernando Chaves <fernando.chaves16@hotmail.com>
2021-02-10 05:24:39 -0300

    added card container function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const openBtn = document.getElementById('open-btn');
+const addCardContainer = document.getElementById('add-card-container');
+
+
+
+// open add card container
+openBtn.addEventListener('click', () => {
+    addCardContainer.classList.add('active');
+});
