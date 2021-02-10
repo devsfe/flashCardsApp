71217c4dada1a914ab59bdb09b5c96f7287e837d
Fernando Chaves <fernando.chaves16@hotmail.com>
2021-02-10 07:46:41 -0300

    add new card & show cards dom functions created

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 const openBtn = document.getElementById('open-btn');
-const closeBtn = document.getElementById('close-btn')
+const closeBtn = document.getElementById('close-btn');
+const addCardForm = document.getElementById('form-add-card');
 const addCardContainer = document.getElementById('add-card-container');
+const cardContainer = document.getElementById('card-container');
+const cardArr = [];
 
 
+// add new card
+function addNewCard(e) {
+    e.preventDefault();
+    
+    let question = document.getElementById('question').value;
+    let answer = document.getElementById('answer').value;
+    const cardContent = {question, answer};
+
+    cardArr.push(cardContent);
+
+    showCardsDom();
+}
+
+// show cards in DOM
+function showCardsDom() {
+    cardArr.forEach((card, index, array) => {
+        if(index === array.length - 1) {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        cardElement.innerHTML = 
+            `<p class="card-question">${card.question}</p>
+            <hr>
+            <p class="card-answer">${card.answer}</p>
+            <button class="show-answer-btn">Show answer</button>`
+            
+        cardContainer.appendChild(cardElement);
+        }
+    });
+}
 
 // open add card container
 openBtn.addEventListener('click', () => {
@@ -13,3 +45,5 @@ openBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     addCardContainer.classList.remove('active');
 });
+
+addCardForm.addEventListener('submit', addNewCard);
